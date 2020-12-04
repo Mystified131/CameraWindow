b80bdeae2c6d0eecf443175ae97d7ce07c328bba
mystified131 <mystifiedthomas@gmail.com>
2020-12-04 05:50:46 -0600

    Tweaking

--- Camera.py
+++ Camera.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import cv2 as cv
  
 capture = cv.VideoCapture(0)
@@ -11,17 +10,4 @@ while True:
  
 capture.release()
 cv.destroyAllWindows()
-=======
-import cv2 as cv
- 
-capture = cv.VideoCapture(0)
- 
-while True:
-    isTrue,frame = capture.read()
-    cv.imshow('Video',frame)
-    if cv.waitKey(20) & 0xFF==ord('d'):
-        break
- 
-capture.release()
-cv.destroyAllWindows()
->>>>>>> d5afd32200e948841f7274181de40d4be8ec639f
+
